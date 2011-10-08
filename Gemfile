d276e6386a71e629d5f46f4ae3d6ad6fe1a5a275
Julio Protzek <julioprotzek@gmail.com>
2011-10-08 15:29:02 -0300

    rails+activeadmin instalados e configurados

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+
+gem 'sqlite3', group: :development
+
+gem 'pg', group: :production
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem "meta_search",    '>= 1.1.0.pre'
+gem 'jquery-rails'
+gem 'activeadmin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '480cc910182cc8c198777ef917141ad00274f536d82622e2518c5350648dca44abb5bcfbc3ba9ef485c3b06e3983bfd76ed4b06efa8b091b3ec9e0f67c39c4a6'
